60c4adafe3abb41c6b82fe78e55214a3a5b334cf
Leandro Menoni <144243042+MenoniJesus@users.noreply.github.com>
2024-07-22 23:47:11 -0300

    Update Ex6.asm

--- Ex6/Ex6.asm
+++ Ex6/Ex6.asm
@@ -9,18 +9,18 @@ li $t2, 0 #maior valor
 la $t3, vetor1
 
 loop:
-bge $t0, $t1, exit_loop
-lw $t4, 0($t3)
-bge $t2, $t4, continua_loop
-move $t2, $t4
+    bge $t0, $t1, exit_loop
+    lw $t4, 0($t3)
+    bge $t2, $t4, continua_loop
+    move $t2, $t4
 j loop
 
 continua_loop:
-add $t0, $t0, 1
-add $t3, $t3, 4
+    add $t0, $t0, 1
+    add $t3, $t3, 4
 j loop
 
 exit_loop:
-add $v0, $zero, 1
-move $a0, $t2
-syscall
+    add $v0, $zero, 1
+    move $a0, $t2
+    syscall
