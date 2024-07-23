77e633defcf7f403db2927c0d47656ef019c1f89
Leandro Menoni <144243042+MenoniJesus@users.noreply.github.com>
2024-07-22 23:46:09 -0300

    Update Ex5.asm

--- Ex5/Ex5.asm
+++ Ex5/Ex5.asm
@@ -1,6 +1,6 @@
 .data
 
-vetor1: .space 400 # espaço para 100 palavras
+vetor1: .space 400 # espaÃ§o para 100 palavras
 
 .text
 
@@ -9,39 +9,39 @@ li $t1, 100 #n
 la $t2, vetor1
 
 loop:
-bge $t0, $t1, exit_loop
-mul $t4, $t0, $t0
-sw $t4, 0($t2)
+    bge $t0, $t1, exit_loop
+    mul $t4, $t0, $t0
+    sw $t4, 0($t2)
 
-add $v0, $zero, 1
-move $a0, $t0
-syscall
+    add $v0, $zero, 1
+    move $a0, $t0
+    syscall
 
-li $a0, 32
-li $v0, 11
-syscall
+    li $a0, 32
+    li $v0, 11
+    syscall
 
-li $a0, 45
-li $v0, 11
-syscall
+    li $a0, 45
+    li $v0, 11
+    syscall
 
-li $a0, 32
-li $v0, 11
-syscall
+    li $a0, 32
+    li $v0, 11
+    syscall
 
-add $v0, $zero, 1
-move $a0, $t4
-syscall
+    add $v0, $zero, 1
+    move $a0, $t4
+    syscall
 
-li $a0, 10
-li $v0, 11
-syscall
+    li $a0, 10
+    li $v0, 11
+    syscall
 
-add $t0, $t0, 1
-add $t2, $t2, 4
+    add $t0, $t0, 1
+    add $t2, $t2, 4
 j loop
 
 
 exit_loop:
-li $v0, 10
-syscall
+    li $v0, 10
+    syscall
